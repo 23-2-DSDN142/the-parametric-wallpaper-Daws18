48e293396a0211934d6ac6b1e8e695b053ca01e5
Daws18 <dawsonfernandes18@gmail.com>
2023-07-24 10:12:08 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -15,9 +15,36 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(165, 230, 250); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  noStroke();
+  fill(255);
+  ellipse(100, 100, 60, 45); //head oval 
+//ellipse(x, y, w, [h])
+
+
+rect(80, 110, 30, 40);//head rectangle 
+stroke(12, 13, 12); //mouth  
+strokeWeight(2);
+rect(85, 125, 20, 20);
+
+fill(0);
+ellipse(85, 100, 10, 15);//left eye 
+ellipse(105, 100, 10, 15);//right eye 
+
+fill(200)
+ellipse(85, 100, 10, 10);//left eye shadow
+ellipse(105, 100, 10, 10);//right eye shadow
+
+
+line(95, 125, 95, 145);//teeth first line left 
+line(90, 125, 90, 145);//teeth middle line  
+line(100, 125, 100, 145);//teeth last line right 
+line(85, 135, 105, 135);//teeth horrizontal line 
+
+fill(0)
+ellipse(90, 115, 2, 5);// nostril
+ellipse(95, 115, 2, 5);// nostril
 }
